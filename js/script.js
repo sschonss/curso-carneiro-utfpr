8dc3f76b215dbb5bf4e193ab1ce281d8dc6e3129
MarceloVichar <marcos.marcelo43@gmail.com>
2023-06-05 11:55:51 -0300

    proposta de js com o array de tarefas salvos no script

--- js/script.js
+++ js/script.js
@@ -1,37 +1,72 @@
-function salvarTarefa(){
-    let nome_tarefa = document.getElementById('tarefa').value;
+var tasks = [];
     
-    if (nome_tarefa == '') {
-        alert('Informe o nome da tarefa!');
-        return false;
-    } else {
-        let html = gerarLinhaTabela(nome_tarefa);
-        document.getElementById("tabela_tarefas").innerHTML += html;
-        document.getElementById('tarefa').value = '';
-    }
-}
-
-
-function gerarLinhaTabela(nome_tarefa) {
-    
-    let html = '<tr>';
-    html += '<td>' + nome_tarefa + '</td>';
-    html += '<td>';
-    html += '<button type="button" onclick="editarTarefa(this)">Editar</button>';
-    html += '<button type="button" onclick="excluirTarefa(this)">Excluir</button>';
-    html += '</td>';
-    html += '</tr>';
-
-    return html;
-}
-
-function editarTarefa(elemento) {
-    let tarefa = elemento.parentNode.parentNode.cells[0].innerHTML;
-    document.getElementById('tarefa').value = tarefa;
-    elemento.parentNode.parentNode.remove();
-}
-
-
-function excluirTarefa(elemento) {
-    elemento.parentNode.parentNode.remove();
-}
+    // Função para atualizar a lista de tarefas
+    function updateTaskList() {
+      var taskList = document.getElementById("task-list");
+      taskList.innerHTML = "";
+      
+      tasks.forEach(function(task, index) {
+        var taskRow = document.createElement("tr");
+        
+        var taskCheckboxCell = document.createElement("td");
+        var taskCheckbox = document.createElement("input");
+        taskCheckbox.type = "checkbox";
+        taskCheckbox.checked = task.completed;
+        taskCheckbox.addEventListener("change", function() {
+          toggleTaskStatus(index);
+        });
+        taskCheckboxCell.appendChild(taskCheckbox);
+        
+        var taskTextCell = document.createElement("td");
+        var taskText = document.createElement("input");
+        taskText.type = "text";
+        taskText.value = task.title;
+        taskText.readOnly = true;
+        taskTextCell.appendChild(taskText);
+        
+        var taskDeleteCell = document.createElement("td");
+        var taskDeleteButton = document.createElement("button");
+        taskDeleteButton.textContent = "Excluir";
+        taskDeleteButton.addEventListener("click", function() {
+            deleteTask(index);
+          });
+          taskDeleteCell.appendChild(taskDeleteButton);
+          
+          taskRow.appendChild(taskCheckboxCell);
+          taskRow.appendChild(taskTextCell);
+          taskRow.appendChild(taskDeleteCell);
+          
+          taskList.appendChild(taskRow);
+        });
+      }
+      
+      // Função para criar uma nova tarefa
+      function createTask(event) {
+        event.preventDefault();
+        var taskInput = document.getElementById("task-input");
+        
+        var newTask = {
+          title: taskInput.value,
+          completed: false
+        };
+        
+        tasks.push(newTask);
+        
+        taskInput.value = "";
+        
+        updateTaskList();
+      }
+      
+      // Função para alternar o status de uma tarefa
+      function toggleTaskStatus(index) {
+        tasks[index].completed = !tasks[index].completed;
+      }
+      
+      // Função para excluir uma tarefa
+      function deleteTask(index) {
+        tasks.splice(index, 1);
+        updateTaskList();
+      }
+  
+      var taskForm = document.getElementById("task-form");
+      taskForm.addEventListener("submit", createTask);
